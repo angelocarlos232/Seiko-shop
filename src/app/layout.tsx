2056f03f5090cd21bf633bddab83d479366fd653
Christian Angelo S. Carlos <christian.carlos@seven-gen.com>
2024-10-02 10:48:47 +0800

    Changes: - Initially created navbar - Changed font of the web app - created layouts for auth and main - added shadcn - added button component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,30 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const markPro = localFont({
+  src: [
+    {
+      path: './fonts/Mark Pro.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Mark Pro Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Mark Pro Heavy.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Mark Pro Bold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-mark-pro',
 });
 
 export const metadata: Metadata = {
@@ -24,10 +39,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={markPro.variable}>
+      <body className="font-sans antialiased">
         {children}
       </body>
     </html>
