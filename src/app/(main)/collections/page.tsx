1efa20fe8cd38475f4d91ca9a95c7ebcf67c0dc4
Christian Angelo S. Carlos <angelocarlos232@gmail.com>
2024-10-12 19:14:03 +0800

    changes

--- src/app/(main)/collections/page.tsx
+++ src/app/(main)/collections/page.tsx
@@ -1,19 +1,8 @@
-"use client";
-import React, { useState } from 'react';
-import FilterBar from '@/components/custom/filterbar';
+  
 
 export default function CollectionsPage() {
-  const [filters, setFilters] = useState<unknown>({});
-
-  const handleFilterChange = (newFilters: unknown) => {
-    setFilters(newFilters);
-    console.log(filters);
-    // Here you would typically fetch or filter your watch data based on the new filters
-  };
-
   return (
     <div>
-      <FilterBar onFilterChange={handleFilterChange} />
     </div>
-  )
+  );
 }
